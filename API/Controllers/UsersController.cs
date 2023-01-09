0946065674c000563d179753ca95d37c4b0aee97
Tokopukoy77 <an.yurchenko.an@gmail.com>
2023-01-09 11:33:54 +0200

    authentication

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,31 +1,32 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-[ApiController]
-[Route("api/[controller]")]
-public class UsersController : ControllerBase
+[Authorize]
+public class UsersController : BaseApiController  
 {
-  private readonly DataContext _context;
-  public UsersController(DataContext context)
-  {
-    _context = context;
-  }
+	private readonly DataContext _context;
+	public UsersController(DataContext context)
+	{
+		_context = context;
+	}
 
-  [HttpGet]
-  public async Task<ActionResult<IEnumerable<AppUser>>> Index()
-  {
-    var users = await _context.Users.ToListAsync();
-    return users;
-  }
+	[AllowAnonymous]
+	[HttpGet]
+	public async Task<ActionResult<IEnumerable<AppUser>>> Users()
+	{
+		var users = await _context.Users.ToListAsync();
+		return users;
+	}
 
-  [HttpGet("{id}")]
-  public async Task<ActionResult<AppUser>> GetUser(int id)
-  {
-    var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
-    return user;
-  }
+	[HttpGet("{id}")]
+	public async Task<ActionResult<AppUser>> GetUser(int id)
+	{
+		var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
+		return user;
+	}
 }
