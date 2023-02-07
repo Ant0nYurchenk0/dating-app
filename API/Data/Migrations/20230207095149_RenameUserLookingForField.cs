2f3179d8b7512ae7be00885fb8a20f8980bb2c7a
Tokopukoy77 <an.yurchenko.an@gmail.com>
2023-02-07 12:49:54 +0200

    updating resources

---
+++ API/Data/Migrations/20230207095149_RenameUserLookingForField.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class RenameUserLookingForField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LookigFor",
+                table: "Users",
+                newName: "LookingFor");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LookingFor",
+                table: "Users",
+                newName: "LookigFor");
+        }
+    }
+}
